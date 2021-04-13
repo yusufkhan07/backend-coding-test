1c4221199d2136cd2a53482dbfe896ffb62ff43e
Yousuf Khan <yusufkhanjee@gmail.com>
2021-04-13 11:01:46 +0500

    config module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -6,7 +8,7 @@ import { AuthModule } from './auth';
 import { UsersModule } from './users';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
