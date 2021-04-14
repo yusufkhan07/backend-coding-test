3d7ebaf9dc8c5dd74cd6f2946379715c9188ed3b
Yousuf Khan <yusufkhanjee@gmail.com>
2021-04-14 11:49:30 +0500

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 // import * as csurf from 'csurf';
 import * as helmet from 'helmet';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.use(helmet());
   // app.use(csurf());
+  app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setContact('Yousuf Khan', null, 'yusufkhanjee@gmail.com')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
-
 bootstrap();
