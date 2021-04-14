f775c21619c18a8455dbcd78930915e237fe1dec
Yousuf Khan <yusufkhanjee@gmail.com>
2021-04-14 15:44:34 +0500

    Added auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 
 import { FirebaseStrategy } from './firebase.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [PassportModule],
   providers: [AuthService, FirebaseStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
